cb56d56d16913414e838f1a18b9bdcf7798987c3
jol79 <jolhuck@gmail.com>
2021-02-19 23:14:34 +0200

    replace the SECRET_KEY from settings.py

--- TFPT_trainee/settings.py
+++ TFPT_trainee/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+from .secretcode import SECRETKEY
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9+(!hrl)bn17pobq0a7jv4-yjsh4n=ab7bo2ix)@r0m+1v0nt6'
+SECRET_KEY = SECRETKEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
