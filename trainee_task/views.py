e2377a406c169413bbe072ff3ae356745df83c5b
jol79 <jolhuck@gmail.com>
2021-02-22 18:32:13 +0200

    close #4

--- trainee_task/views.py
+++ trainee_task/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import	HttpResponse
 
 from .models import User
 import random as rd
@@ -8,7 +9,10 @@ import random as rd
  Views
 '''
 def magiclink_view(request): # increment the value of counter by 1 and show the email/total
-	obj = User.objects.get(user_id=1)
+	rec_user_id = request.GET.get('email') # retrieve the user_id from the url
+	rec_token = request.GET.get('token') # retrieve the token from the url
+
+	obj = User.objects.get(email=rec_user_id, token=rec_token) # get the data related with a user
 	obj.counter = increment(obj.counter)
 	context = {
 		'email': obj.email, 
